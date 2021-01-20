f3566291ec910cea0155a0ea4607def71802c418
chris.kearse <chris.kearse@gmail.com>
2021-01-20 11:13:17 -0800

    Adding defined routes to server.js

--- server.js
+++ server.js
@@ -2,10 +2,20 @@ const express = require('express');
 const parser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
+const connectDB = require('./config/db');
+
+//Connect DB
+connectDB();
 
 app.get('/', (req, res) => {
   res.send("API Running");
 })
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 app.listen(port, ()=>{
   console.log(`Listening on port: ${port}`)
